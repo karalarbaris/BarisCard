720475e5da647e385a2e04f2bdc5d20e47fdf564
Baris <karalarbaris@gmail.com>
2021-07-10 17:33:50 +0200

    Initial Commit

---
+++ BarisCard/BarisCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarisCardApp.swift
+//  BarisCard
+//
+//  Created by Baris Karalar on 10.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct BarisCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarisCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarisCard
+//
+//  Created by Baris Karalar on 10.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
