a2e0da68025a227a8190f2d55fad90f52d51b059
Baris <karalarbaris@gmail.com>
2021-07-10 18:53:18 +0200

    UI

--- BarisCard/ContentView.swift
+++ BarisCard/ContentView.swift
@@ -9,8 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.07, green: 0.54, blue: 0.65, opacity: 0.8)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("baris")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200, height: 230, alignment: .center)
+                    .clipShape(Capsule())
+                    .overlay(Capsule().stroke(Color.white, lineWidth: 3))
+                
+                
+                Text("Barış Karalar")
+                    .font(Font.custom("Pacifico-Regular", size:40))
+                    .foregroundColor(Color.white)
+                    .bold()
+                
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                
+                InfoView(text: "+49 1573 6729178", imageName: "phone.fill")
+                InfoView(text: "karalarbaris@gmail.com", imageName: "envelope.fill ")
+                
+            }
+            
+                
+            
+        }
     }
 }
 
---
+++ BarisCard/InfoView.swift
@@ -0,0 +1,39 @@
+//
+//  InfoView.swift
+//  BarisCard
+//
+//  Created by Baris Karalar on 10.07.2021.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    var text : String
+    var imageName : String
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .frame(height:50)
+                .foregroundColor(.white)
+            HStack {
+                Image(systemName: imageName )
+                    .foregroundColor(Color(red: 0.07, green: 0.54, blue: 0.65, opacity: 0.8))
+                
+                Text(text)
+                    .bold()
+            }
+            
+        }
+        .padding()
+    }
+}
+
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "3242323234", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
